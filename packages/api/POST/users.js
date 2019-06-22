b76421660838891f635a2128231022d4a16e190c
alaspoorhenry <123henryxx@gmail.com>
2019-06-22 18:58:51 -0400

    addeed db

--- packages/api/POST/users.js
+++ packages/api/POST/users.js
@@ -1,6 +1,5 @@
 var express = require("express");
 var router = express.Router();
-
 const user = require("../schema/user.js");
 const key = require("../schema/secret.js");
 
--- packages/api/app.js
+++ packages/api/app.js
@@ -4,5 +4,27 @@ const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const mongo = require("mongodb");
+
+var mongoClient = mongo.MongoClient;
+var url =
+  "mongodb+srv://admin:12345@firstcluster-trvlr.azure.mongodb.net/test?retryWrites=true&w=majority";
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoClient.connect(url, function(err, db) {
+  if (err) {
+    console.log("ERROR: could not connect:", err);
+    return;
+  }
+  console.log("Database created!");
+  db.close();
+});
 
 app.use(bodyParser.json());
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log("Started on :", PORT);
+});
