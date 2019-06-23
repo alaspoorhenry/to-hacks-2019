e6e0b18e3aae48851bda35fdb5d0e5f85a6e3a67
alaspoorhenry <123henryxx@gmail.com>
2019-06-23 04:43:50 -0400

    delete

---
+++ frontend/backend/DELETE.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const User = require("./modelUser.js");
+const Media = require("./modelMedia.js");
+const middleware = require("./middleware");
+
+router.delete("/media/:mediaID", async function(req, res) {
+  let toDelete = await db.collection("media").find({ _id: req.params.mediaID });
+  if (toDelete === null) {
+    return res.status(500).end("No element to delete");
+  }
+  db.collection("media").remove({ _id: req.params.mediaID }, { justOne: true });
+});
