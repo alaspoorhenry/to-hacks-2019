16ef03775490cc587612c81b7842ee6403e20c0d
alaspoorhenry <123henryxx@gmail.com>
2019-06-23 02:59:03 -0400

    removed jumk

--- frontend/backend/PATCH.js
+++ frontend/backend/PATCH.js
@@ -3,8 +3,3 @@ const router = express.Router();
 const User = require("./modelUser.js");
 const Media = require("./modelMedia.js");
 const middleware = require("./middleware");
-
-router.patch("/user/:userID", async function(req, res) {
-  try {
-  } catch (err) {}
-});
